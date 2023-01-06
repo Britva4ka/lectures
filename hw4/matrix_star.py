264fe0a8a3c07e13ae2cd6990724fa21efa668db
Britva4ka <britva4ka.number1@gmail.com>
2023-01-06 19:11:54 +0300

    Трохи матрицю доробив

--- hw4/matrix_star.py
+++ hw4/matrix_star.py
@@ -33,13 +33,32 @@ matrix = [
     [4, 5, 6],
     [7, 8, 9]
 ]
-print(matrix)
+mat2 = [
+    [7, 4, 1],
+    [8, 5, 2],
+    [9, 6, 3]
+]
 result = []
 for x in range(3):   # Ідея в тому що range(3) це як список із потрібних індексів (0,1,2)
     line = []      #Оновляємо стрінг кожного  разу
     for i in matrix[::-1]:    #Нам треба перевернути матрицю щоб брати числа знизу спочатку типо 741 а не 147
         line.append(i[x])
     result.append(line)
-print(result)
+#print(result)
+titles_list = ["input", "needed", "result"]
 
+for i in range(len(titles_list)):
+    titles_list[i] = titles_list[i].ljust(13, " ")
+
+print(*titles_list) #Не знав шо можна розпаковувать списки
+
+for i, j, k in zip(matrix, mat2, result): #Трохи зрозумів, але не дуже. як працює функція ZIP
+    print(i," | ", j, " | ", k)
+
+
+print("*" * 38)
+if result == mat2:
+    print("Problem solved!")
+else:
+    print("Wrong answer!")
 
